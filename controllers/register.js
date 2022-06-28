28976423653b9dbfb3a4b1e320a0bb3df8f5450e
samuelymh <103726434+samuelymh@users.noreply.github.com>
2022-06-28 19:20:23 +0800

    feat: Added validation for user registration inputs

--- controllers/register.js
+++ controllers/register.js
@@ -1,5 +1,13 @@
 const handleRegister = (req, res, db, bcrypt) => {
   const { name, email, password } = req.body;
+
+  // We want to validate the user inputs on both frontend and backend separately.
+  // We don't want to trust the frontend to make correct validations.
+  // This way it's more robust.
+  if(!name || !email || !password){
+    return res.status(400).json('Incorrect submission format');
+  }
+
   bcrypt.hash(password, null, null, function(err, hash) {
     // Store hash in your password DB.
     // We use a transaction when we have to manipulate >= 2 things
